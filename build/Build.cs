37a1e2ba7c6edffa03611ab39f5bc021fe20e0b3
Jens Schadron <jens.schadron@rmdy.be>
2022-11-30 01:44:55 +0100

    Add initial pipeline setup

--- build/Build.cs
+++ build/Build.cs
@@ -1,17 +1,15 @@
-using System;
-using System.Linq;
 using Nuke.Common;
 using Nuke.Common.CI;
-using Nuke.Common.Execution;
 using Nuke.Common.IO;
 using Nuke.Common.ProjectModel;
-using Nuke.Common.Tooling;
+using Nuke.Common.Tools.DotNet;
+using Nuke.Common.Tools.GitVersion;
 using Nuke.Common.Utilities.Collections;
-using static Nuke.Common.EnvironmentInfo;
 using static Nuke.Common.IO.FileSystemTasks;
-using static Nuke.Common.IO.PathConstruction;
+using static Nuke.Common.Tools.DotNet.DotNetTasks;
 
-class Build : NukeBuild
+[ShutdownDotNetAfterServerBuild]
+partial class Build : NukeBuild
 {
     /// Support plugins are available for:
     ///   - JetBrains ReSharper        https://nuke.build/resharper
@@ -23,22 +21,64 @@ class Build : NukeBuild
 
     [Parameter("Configuration to build - Default is 'Debug' (local) or 'Release' (server)")]
     readonly Configuration Configuration = IsLocalBuild ? Configuration.Debug : Configuration.Release;
+    
+    [Solution(GenerateProjects = true)] readonly Solution Solution;
+
+    [GitVersion] readonly GitVersion GitVersion;
+
+    AbsolutePath SourceDirectory => RootDirectory / "src";
+    AbsolutePath ArtifactsDirectory => RootDirectory / "artifacts";
 
     Target Clean => _ => _
         .Before(Restore)
         .Executes(() =>
         {
+            SourceDirectory.GlobDirectories("**/bin", "**/obj").ForEach(DeleteDirectory);
+            EnsureCleanDirectory(ArtifactsDirectory);
         });
 
     Target Restore => _ => _
         .Executes(() =>
         {
+            DotNetRestore(s => s
+                .SetProjectFile(Solution)
+                .EnableDeterministic()
+                .EnableContinuousIntegrationBuild());
         });
 
     Target Compile => _ => _
         .DependsOn(Restore)
         .Executes(() =>
         {
+            DotNetBuild(s => s
+                .EnableNoRestore()
+                .SetProjectFile(Solution)
+                .SetConfiguration(Configuration)
+                .SetDeterministic(IsServerBuild)
+                .SetContinuousIntegrationBuild(IsServerBuild)
+                .SetAssemblyVersion(GitVersion.AssemblySemVer)
+                .SetFileVersion(GitVersion.AssemblySemFileVer)
+                .SetInformationalVersion(GitVersion.InformationalVersion));
         });
 
+    Target Pack => _ => _
+        .DependsOn(Clean, Compile)
+        .Produces(ArtifactsDirectory / "*.nupkg")
+        .Executes(() =>
+        {
+            DotNetPack(s => s
+                .EnableNoRestore()
+                .EnableNoBuild()
+                .SetProject(Solution.Localizer_SourceGenerator)
+                .SetConfiguration(Configuration)
+                .SetVersion(GitVersion.NuGetVersion)
+                .SetOutputDirectory(ArtifactsDirectory));
+        });
+
+    Target Publish => _ => _
+        .DependsOn(Pack)
+        .Executes(() =>
+        {
+            // NOP
+        });
 }

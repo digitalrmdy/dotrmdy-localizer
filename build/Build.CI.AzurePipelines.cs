73b18be9c0eab634dcd3ff0e12a21ed88856c059
Jens Schadron <jens.schadron@rmdy.be>
2023-08-04 11:43:59 +0200

    Disable PR pipeline triggers

--- build/Build.CI.AzurePipelines.cs
+++ build/Build.CI.AzurePipelines.cs
@@ -5,8 +5,7 @@ using Nuke.Common.CI.AzurePipelines;
     AzurePipelinesImage.UbuntuLatest,
     AutoGenerate = false,
     FetchDepth = 0,
-    TriggerBatch = true,
-    PullRequestsBranchesInclude = new[] { "main" },
+    TriggerDisabled = true,
     InvokedTargets = new[] { nameof(Pack) },
     CacheKeyFiles = new string[0],
     CachePaths = new string[0])]
